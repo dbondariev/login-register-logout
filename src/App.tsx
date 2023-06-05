5e2b032435fc27521c258fbaeae6fa2a8d738c37
Dima Bondariev <dima.bondariev@gmail.com>
2023-06-05 19:03:16 +0300

    made signin and sgn up forms with mui without styles yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import SignInForm from "./components/auth/SignInForm";
+import SignUpForm from "./components/auth/SignUpForm";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SignInForm from "../components/auth/SignInForm";
+import SignUpForm from "../components/auth/SignUpForm";
+
+function AuthPage() {
+  return (
+    <>
+      {/* <SignInForm /> */}
+    </>
+  );
+}
+
+export default AuthPage;
